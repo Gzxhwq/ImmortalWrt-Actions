247e9f9c9c3121cff6e8f58864558e88c0e0cc73
Gzxhwq <gzxhwq@gmail.com>
2021-10-23 09:08:41 +0800

    fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,4 +31,4 @@ wget https://ispip.clang.cn/othernet_cidr.txt -O feeds/luci/applications/luci-ap
 sed -i 's/--cache --cache-min-ttl=3600/--cache --cache-min-ttl=600/g' ./feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
 
 # Convert zh-cn to zh_Hans
-bash <(curl -SsL  https://github.com/immortalwrt/build-scripts/raw/master/convert_translation.sh)
+bash <(curl -L  https://github.com/immortalwrt/build-scripts/raw/master/convert_translation.sh)
