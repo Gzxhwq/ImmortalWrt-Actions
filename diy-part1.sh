b44be8a16c525f50d5f02e0067c7e97c9747054c
Gzxhwq <gzxhwq@gmail.com>
2023-02-21 18:24:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,10 @@
 #Add external packages
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/others/luci-app-amlogic
 svn co https://github.com/QiuSimons/openwrt-mos/trunk/v2ray-geodata package/others/v2ray-geodata
-svn co https://github.com/Gzxhwq/openwrt-passwall/branches/packages-me/sing-box package/others/sing-box
+
+svn co https://github.com/Gzxhwq/openwrt-passwall/branches/sing-box-git/sing-box package/others/sing-box
 sed -i '/iptables-mod-socket/d' ./package/others/sing-box/Makefile
+#svn co https://github.com/Gzxhwq/openwrt-passwall/branches/xray-core-git/xray-core package/others/xray-core
+
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci-reality/luci-app-passwall package/others/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/others/luci-app-passwall2
